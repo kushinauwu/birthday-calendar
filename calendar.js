77243a760556fa0f4835eaa5ac9c39b45612dee8
Deepashree Deshmukh <18piscean@gmail.com>
2019-03-08 20:48:35 +0530

    mouseover and mouseout events

--- calendar.js
+++ calendar.js
@@ -1,4 +1,4 @@
-function birthday_click(clicked_id) {
+function birthday_active(clicked_id) {
     /*
     Get iD of clicked element and add -birthday to it, which is the ID format of the pcitture of the person whose birthdate is clicked
     */
@@ -6,7 +6,25 @@ function birthday_click(clicked_id) {
     console.log(birthdayPerson);
 
     /*
-    unblur the birthday person's image
+    unblur the birthday person's image on mouseover
     */
+
     document.getElementById(birthdayPerson).style.filter = "blur(0px)";
+    console.log("unblurred");
+
+}
+
+function birthday_inactive(clicked_id) {
+    /*
+    Get iD of clicked element and add -birthday to it, which is the ID format of the pcitture of the person whose birthdate is clicked
+    */
+    const birthdayPerson = clicked_id + "-birthday";
+    console.log(birthdayPerson);
+
+    /*
+    blur the birthday person's image on mouseout
+    */
+
+    document.getElementById(birthdayPerson).style.filter = "blur(5px)";
+    console.log("blurred");
 }
